acff84c3e1feb1ba9ab77b2c4062d3c87cd05df8
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-11-23 18:46:15 +0100

    controller finished

--- src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
+++ src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +30,25 @@ public class SongController {
     @PostMapping("/song/")
     public ResponseEntity<Song> createSong(@RequestBody Song song){
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(song));
+    }
+
+    @PutMapping("/song/{id}")
+    public ResponseEntity<Song> customSong(@RequestBody Song song, @PathVariable Long id){
+        return ResponseEntity.of(repository.findById(id)
+                .map(old -> {
+                    old.setYear(song.getYear());
+                    old.setTitle(song.getTitle());
+                    old.setAlbum(song.getAlbum());
+                    return Optional.of(repository.save(old));
+                })
+                .orElse(Optional.empty())
+        );
+    }
+
+    @DeleteMapping("/song/{id}")
+    public ResponseEntity<Song> deleteSong(@PathVariable Long id){
+        if(repository.existsById(id))
+            repository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
